6fe24353558910d8be5796b09395a04c611579eb
Alexander <alexandergeraskov1@gmail.com>
2021-11-10 22:16:25 +0300

    Added user registration form

--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import Homepage
+from .views import Homepage, register
+from . import views
 
 urlpatterns = [
-    path('', Homepage.as_view(), name = 'homepage')
+    path('', Homepage.as_view(), name = 'homepage'),
+    path('register/', views.register, name='register')
 ]
--- main/views.py
+++ main/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
 from .models import Review
+from django.contrib.auth.forms import UserCreationForm
 
 class Homepage(ListView):
     model = Review
     template_name = 'main/index.html'
     ordering = ['-rating']
+
+def register(request):
+    form = UserCreationForm
+    return render(request, 'main/register.html', context={'form':form})
