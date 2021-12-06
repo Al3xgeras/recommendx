48435a1e817f390f7f617a9b4d2b57626c7109c0
Alexander <alexandergeraskov1@gmail.com>
2021-12-06 15:02:55 +0300

    Default png picture fix

--- users/models.py
+++ users/models.py
@@ -1,11 +1,11 @@
 from django.db import models
-from cloudinary.models import CloudinaryField
+from cloudinary.models import CloudinaryField, CloudinaryImage
 from django.contrib.auth.models import User
 from PIL import Image
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     image = CloudinaryField('media')
-    
+    default_image = CloudinaryImage('default.png')
     def __str__(self):
         return f'{self.user.username} Profile'
